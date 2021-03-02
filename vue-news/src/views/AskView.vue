142b1ae8508ef14cacda132ce824d79082ba06a9
eunhye8767 <leh8767@gmail.com>
2021-03-02 16:11:20 +0900

    LV-04 Vue.js 리팩토링 1 - 02 질문, 구직 리스트 스타일링 (실습)

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,14 +1,27 @@
 <template>
   <div>
-    <p v-for="item in askItems">
-      <router-link v-bind:to="`/item/${item.id}`">{{ item.title}}</router-link>
-      <!-- <a v-bind:href="item.url">
-        {{ item.title}}
-      </a> -->
-      <small>
-        {{ item.time_ago}} by {{ item.user }}
-        </small>
-    </p>
+    <ul class="news-list">
+      <li v-for="item in askItems" class="post">
+        <!-- 포인트 영역 -->
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <!-- 기타 정보 영역 -->
+        <div>
+          <p class="news-title">
+            <router-link v-bind:to="`/item/${item.id}`">
+              {{ item.title}}
+            </router-link>
+          </p>
+          <small class="link-text">
+            {{ item.time_ago}} by 
+            <router-link v-bind:to="`/user/${item.user}`" class="link-text">
+              {{ item.user }}
+            </router-link>  
+          </small>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -27,6 +40,29 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.news-list {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+.post {
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282
+}
 </style>
